4ee331304f60b804b3ba0656977e81495aed6580
Xiangrui Dong <xiangrui.dong@thoughtworks.com>
2020-12-21 16:32:03 +0800

    [xdong] step-2: Generate users' account

---
+++ src/main/java/com/example/config/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package com.example.config;
+
+import com.example.entity.Role;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .passwordEncoder(new BCryptPasswordEncoder())
+                .withUser("user")
+                .password(new BCryptPasswordEncoder().encode("password"))
+                .authorities(String.valueOf(Role.USER))
+            .and()
+                .withUser("admin")
+                .password(new BCryptPasswordEncoder().encode("p@ssw0rd"))
+                .authorities(String.valueOf(Role.ADMIN));
+    }
+}
