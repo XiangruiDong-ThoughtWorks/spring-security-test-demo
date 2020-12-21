c2b56adf04afecd8b607fd028867deffe5edaed9
Xiangrui Dong <xiangrui.dong@thoughtworks.com>
2020-12-21 17:05:59 +0800

    [xdong] step-3: Set authority requirement for each page

--- src/main/java/com/example/config/SecurityConfiguration.java
+++ src/main/java/com/example/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.example.config;
 import com.example.entity.Role;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -22,5 +23,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .withUser("admin")
                 .password(new BCryptPasswordEncoder().encode("p@ssw0rd"))
                 .authorities(String.valueOf(Role.ADMIN));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/index").permitAll()
+                .antMatchers("/user").hasAuthority(Role.USER.toString())
+                .antMatchers("/admin").hasAuthority(Role.ADMIN.toString());
+        http.formLogin();
     }
 }
